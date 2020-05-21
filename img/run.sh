71076a4c649f6ab715bb5bf2d9e3ed8123a63246
Dipankar Dutta <dutta.dipankar08@gmail.com>
2020-05-21 23:17:34 +0100

    automated chkin

--- img/run.sh
+++ img/run.sh
@@ -1,5 +1,8 @@
 rename 's/\.jpeg/\.jpg/' *.jpeg;
 rename 's/\.JPEG/\.jpg/' *.jpeg;
+rename  's/-/_/' *
+rename  's/-/_/' *
+rename  's/-/_/' *
 mogrify -format jpg *.png
 rm -rf *.png
 rm -rf *.jpeg
